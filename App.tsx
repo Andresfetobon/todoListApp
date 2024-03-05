554ac8917e67f2381db0974ae3e9d30ae84ace91
Andresfetobon <andrestromp777@gmail.com>
2024-03-05 01:58:54 -0500

    Se modifico

--- App.tsx
+++ App.tsx
@@ -1,66 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
-  Dimensions,
+  FlatList,
 } from 'react-native';
+import styles from './Styles';
+import RenderItem from './src/components/RenderItem';
+
+export interface Task {
+  title: string;
+  done: boolean;
+  date: Date;
+}
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = () => {
+    const tmp = [...tasks];
+    const newTask = {
+      title: text,
+      done: false,
+      date: new Date(),
+    };
+    tmp.push(newTask);
+    setTasks(tmp);
+    setText('');
+  };
+
+  const markDone = (task: Task) => {
+    const tmp = [...tasks];
+    const index = tmp.findIndex(el => el.title === task.title)
+    const todo = tasks[index];
+    todo.done = !todo.done;
+    setTasks(tmp);
+  };
+
+  const deleteFunction = () => {};
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Mis Tareas por realizar</Text>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder='Agrega una nueva tarea'
+          onChangeText={(t: string) => setText(t)}
+          value={text}
           style={styles.textIpunt}
         />
-        <TouchableOpacity style={styles.addButton}>
+        <TouchableOpacity style={styles.addButton} onPress={addTask}>
           <Text style={styles.whiteText}>Agregar</Text>
         </TouchableOpacity>
+      </View>
+      <View style={styles.scrollContainer}>
+        <FlatList
+          data={tasks}
+          renderItem={({ item }) => (
+            <RenderItem
+              item={item}
+              deleteFunction={deleteFunction}
+              markDone={markDone}
+            />
+          )}
+        />
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    color: '#6f6f6f',
-    marginTop: 25,
-  },
-  text: {
-    fontSize: 16,
-    color: '#6f6f6f',
-  },
-  whiteText: {
-    fontSize: 16,
-    color: '#fff',
-  },
-  textIpunt: {
-    borderColor: '#6f6f6f',
-    borderWidth: 1,
-    width: Dimensions.get('screen').width * 0.6,
-    borderRadius: 10,
-    paddingLeft: 10,
-  },
-  inputContainer: {
-    marginTop: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  addButton: {
-    width: Dimensions.get('screen').width * 0.2,
-    backgroundColor: '#5897fb',
-    justifyContent: 'center',
-    padding: 10,
-    alignItems: 'center',
-    borderRadius: 10,
-  },
-});
